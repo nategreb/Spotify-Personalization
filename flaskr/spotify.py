2c368c28c9dc890adc4c86ccd4d278b63aa08fd8
Nate Grebelsky <nate.greb@gmail.com>
2021-05-31 13:19:41 -0400

    ft: add playlist

--- flaskr/spotify.py
+++ flaskr/spotify.py
@@ -1,10 +1,8 @@
 from flask import (
     Flask, render_template, request, Blueprint, redirect, url_for, session
 )
-import os
-import requests
-import secrets
-import string
+import os, requests, secrets, json, string
+from datetime import datetime, timedelta
 
 import pkce
 import spotipy
@@ -33,8 +31,8 @@ def recommend():
             error = 'song is required'    
         
         if error is None:            
-            session.clear()         
-            session['token'] = None                          
+            #session.clear()         
+            #session['token'] = None                          
             session['songs'] = get_recommended_tracks([artist],[genre],[song])
             session['query'] = f'{song} by {artist}'
             if session['songs'] is not None:                
@@ -44,37 +42,48 @@ def recommend():
 
 @bp.route('/playlist', methods=('POST', 'GET'))
 def playlist_generator():
-    if session['token'] is None:
-        if request.method == 'POST':
+    error = False
+    authorized = check_token()
+    if request.method == 'POST':        
+        if authorized:
+            isPublic = True if request.form['playlistType'] == 'true' else False
+            create_playlist(request.form['title'], isPublic)
+        else:
             return redirect(url_for('spotify.authorize'))
-        if request.args.get('state') is not None and request.args.get('code') is not None:
-            login()
-    #check validity of token(has expiration in post request)
-    #check query paramters if error. Show on page if the user didn't log in 
-    return render_template('RecommendPlaylist.html', query=session.get('query'),list=session.get('songs'))
+    if request.args.get('state') is not None and request.args.get('code') is not None:
+        get_token() 
+        authorized = True       
+    elif request.args.get('error') is not None: #there is an error in the url. User didn't authenticate. 
+        error = True
+    else: #there is a valid token. SHOULD GO ABOVE UNDER POST
+        #get_user_id()
+        pass
+    return render_template('RecommendPlaylist.html', query=session.get('query'),list=session.get('songs'), error=error, authorized=authorized)
 
 
 #redirects to Spotify OAuth Page 
 @bp.route('/authorize')
 def authorize():
-    if session['token'] is None:
-        code_verifier, code_challenge = pkce.generate_pkce_pair()
-        session['code_verifier'] = code_verifier
-        session['state'] = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(9))
-        return redirect(
-            f"https://accounts.spotify.com/authorize?" \
-            f"client_id={os.environ['SPOTIPY_CLIENT_ID']}&" \
-            f"response_type=code&" \
-            f"redirect_uri={url_for('spotify.playlist_generator', _external = True)}&" \
-            f"code_challenge_method=S256&" \
-            f"code_challenge={code_challenge}&" \
-            f"state={session['state']}"
-        )
+    #if 'token' not in session:
+    code_verifier, code_challenge = pkce.generate_pkce_pair()
+    session['code_verifier'] = code_verifier
+    session['state'] = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(9))
+    return redirect(
+        f"https://accounts.spotify.com/authorize?" \
+        f"client_id={os.environ['SPOTIPY_CLIENT_ID']}&" \
+        f"response_type=code&" \
+        f"redirect_uri={url_for('spotify.playlist_generator', _external = True)}&" \
+        f"code_challenge_method=S256&" \
+        f"code_challenge={code_challenge}&" \
+        f"state={session['state']}&" \
+        f"scope=playlist-modify-private user-read-private user-read-email"
+    )
 
 
 #after client authorizes the app, gets the token
-def login(): 
-    if request.args.get('state') == session['state'] and not request.args.get('error'):
+#also, calls get_user_id() since we now have the token
+def get_token(): 
+    if request.args.get('state') == session['state'] and not request.args.get('error'): 
         r = requests.post(
             'https://accounts.spotify.com/api/token', 
             data = {
@@ -86,3 +95,82 @@ def login():
             }
         ) 
         session['token'] = r.json()['access_token']    
+        session['token_expiration_time'] = (datetime.now() + timedelta(seconds = r.json()['expires_in'])).replace(tzinfo=None)
+        session['refresh_token'] = r.json()['refresh_token']
+        session['refresh_token_used'] = False
+        #after setting the token, can get user_id for subsequent calls
+        session['user_id'] = requests.get(
+                        'https://api.spotify.com/v1/me',
+                        headers = {
+                            'Accept':'application/json',
+                            'Content-Type':'application/json',
+                            'Authorization':f"Bearer {session['token']}"
+                        }
+                    ).json()['id']
+    
+
+#checks if token is valid. If not, then checks validity of refresh token. Set session accordingly.
+#returns Boolean if valid
+def check_token():
+    if 'token' in session:
+        #post request if the curretn token is invalid and it's not the refresh token.
+        if datetime.now() <= session['token_expiration_time'].replace(tzinfo=None):
+            #return redirect(url)
+            return True
+        elif not session['refresh_token_used']:
+            r = requests.post(
+                'https://accounts.spotify.com/api/token', 
+                data = {
+                    'grant_type':'refresh_token',
+                    'refresh_token':session['refresh_token'],
+                    'client_id':os.environ['SPOTIPY_CLIENT_ID']
+                }
+            )
+            session['token'] = r.json()['access_token']    
+            session['refresh_token_used'] = True
+            session['token_expiration_time'] = (datetime.now() + timedelta(seconds = r.json()['expires_in'])).replace(tzinfo=None)            
+            #return redirect(url)
+            return True
+    else:
+        return False
+
+
+
+#create playlist given user token without using Spotipy 
+#kwargs for description, image, etc.
+#creates and adds songs to the playlist 
+def create_playlist(name, public, **kwargs):
+    description = ''
+    if 'description' in kwargs:
+        description = kwargs['description']
+    #creates a playlist in the user's spotify account with POST.
+    r = requests.post(
+            f"https://api.spotify.com/v1/users/{session['user_id']}/playlists",
+            json = {
+                'name':name,
+                'description':description,
+                'public':public
+            },
+            headers = {
+                'Accept':'application/json',
+                'Content-Type':'application/json',
+                'Authorization':f"Bearer {session['token']}"
+            }
+    )
+    #raises error based on POST 
+    r.status_code
+    r.raise_for_status()
+    #using the playlist's id from the response, add song ids from session.
+    query_tracks = ''
+    #songs is an array of arrays [[title, id]]
+    for song_arr in session['songs']:
+        query_tracks += f"spotify:track:{song_arr[1]},"
+    
+    requests.post(
+        f"https://api.spotify.com/v1/playlists/{r.json()['id']}/tracks?uris={query_tracks}",
+        headers = {
+            'Accept':'application/json',
+            'Content-Type':'application/json',
+            'Authorization':f"Bearer {session['token']}"
+        }
+    )
