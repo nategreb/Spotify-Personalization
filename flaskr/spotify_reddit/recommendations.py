6e631b2ddabbce838f7465e0ded9ab0b8985bc78
Nate Grebelsky <nate.greb@gmail.com>
2021-06-04 19:01:53 -0400

    ft: fixed seed value combinations for recommendations function

--- flaskr/spotify_reddit/recommendations.py
+++ flaskr/spotify_reddit/recommendations.py
@@ -2,23 +2,22 @@ from .spotify_playlists import PrivatePlaylist, client
 
 
 #a list of recommended track ids based on artists 
-#at least one and up to 5 seeds for artists, tracks can be provided
+#Up to 5 seed values may be provided in any combination of seed_artists, seed_tracks and seed_genres.
 #@param: artists, tracks: String[]
 #returns [[String, String]]
 def get_recommended_tracks(artists, tracks, **kwargs):
-    if not artists or not tracks:
+    if len(artists) == 0 or len(tracks) == 0:
         print("at least one of artists, genres, and tracks are required")
     else:
-        genres = seed_genres(artists, 'artist')
         artists = seed_artists(artists)
         tracks = seed_tracks(tracks)
-        if artists and tracks and len(genres) > 0:
+        if len(artists) > 0 and len(tracks) > 0 and len(artists) + len(tracks) <= 5:
             limit = 20
             if len(kwargs) > 0 and 'limit' in kwargs:                
                 limit = kwargs['limit']
             songs = []
-            if limit >= 0 and limit <= 100:
-                for track in client.recommendations(seed_artists=artists, seed_genres=genres, seed_tracks=tracks, limit=limit)['tracks']:
+            if limit > 0 and limit <= 100:
+                for track in client.recommendations(seed_artists=artists, seed_tracks=tracks, limit=limit)['tracks']:
                     songs.append([f"{track['name']} by {track['artists'][0]['name']}", track['id']])
                 return songs
             else: 
@@ -60,15 +59,3 @@ def get_id(query, kind):
     if len(result) > 0 and len(result[kind+'s']['items']) > 0:
         return result[kind+'s']['items'][0]['id']
     return None
-
-
-#get the genre of the query based on list of artists and the Spotify type 
-#(artists: String[], kind: String)
-#returns empty array or up to 5 genres
-def seed_genres(artists, kind):    
-    genres = []
-    for artist in artists:    
-        result = client.search(q=f"{kind}:{artist}", type=kind, limit=1)
-        if len(result) > 0 and len(result[kind+'s']['items']) > 0:
-            genres.extend(result[kind+'s']['items'][0]['genres'])
-    return genres[:5]
--- tests/test_recommendations.py
+++ tests/test_recommendations.py
@@ -6,34 +6,36 @@ import pytest
 from ..flaskr.spotify_reddit.recommendations import *
 
 
-def test_genres():
-    assert seed_genres('', '') == []
-    #following asserts greater or equal to 0 but less than or equal to 5
-    genres = seed_genres(['Kanye West', 'Eminem', 'Logic', 'Drake', 'Eminem'], 'artist')
-    assert len(genres) <= 5
-    assert len(genres) >= 0
-    assert len(seed_genres('', '')) == 0
-
-
 def test_recommended_tracks():
-    assert get_recommended_tracks([''], ['']) is None
+    assert get_recommended_tracks([], []) is None
     #assert missing arguments
     artists = ['Kanye West']
     songs =  ['Monster']
-    assert get_recommended_tracks(artists, None) is None
-    assert get_recommended_tracks(None, songs) is None
-    assert get_recommended_tracks(None, None) is None
+    assert get_recommended_tracks(artists, []) is None
+    assert get_recommended_tracks([], songs) is None
+    assert get_recommended_tracks([], []) is None
     #test limit default, fringe cases, and allowed ones.
     assert len(get_recommended_tracks(artists, songs)) == 20
     assert len(get_recommended_tracks(artists, songs, limit=40)) == 40
     assert get_recommended_tracks(artists, songs, limit=101) == None
     assert get_recommended_tracks(artists, songs, limit=-1) == None
 
+    #test with one input for each
+    songs = ['Monster']
+    artists = ['Kanye West']
+    assert get_recommended_tracks(artists, songs) is not None
+
+    #test with seed combination is greater than 5
     songs = ['Monster','Runaway','Gold Digger','So Appalled','Blood Diamonds','Otis']
     artists = ['Kanye West','Jay Z','Eminem','Tupac','Rick Ross','Beyonce']
+    assert get_recommended_tracks(artists, songs) is None
+
+    #test combination where seeds combination is 5
+    songs = ['Monster','Runaway','Gold Digger']
+    artists = ['Kanye West','Jay Z']
     assert get_recommended_tracks(artists, songs) is not None
+
    
-    
 def test_seed_tracks():\
     #assert more than 5 ids isn't returned
     songs = ['Monster','Monster','Monster','Monster','Monster','Monster']
@@ -45,5 +47,3 @@ def test_seed_artists():
     artists = ['Kanye West','Kanye West','Kanye West','Kanye West','Kanye West','Kanye West']
     assert len(seed_artists(artists)) == 5
     assert len(seed_tracks([])) == 0
-
-    
